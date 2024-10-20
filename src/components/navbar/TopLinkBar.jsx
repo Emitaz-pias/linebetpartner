9b7db1e914fe0fa41b72ac68ea850406a6c9528c
Emtiaz-pias <emtiazpias383@gmail.com>
2024-10-20 21:58:12 +0600

    pushed

--- src/components/navbar/TopLinkBar.jsx
+++ src/components/navbar/TopLinkBar.jsx
@@ -1,39 +1,40 @@
 import React from 'react';
-import androidImage from '../../images/androidImage.png';
-import linkedin from '../../images/linkedIn.png';
-import mail from '../../images/mail.png';
-import telegramLogo from '../../images/telegramLogo.png'
+import { ReactComponent as AndroidImage } from '../../images/android.svg';
+import { ReactComponent as LinkedIn } from '../../images/linkedInLogo.svg';
+import { ReactComponent as Mail } from '../../images/mailLogo.svg';
+import { ReactComponent as TelegramLogo } from '../../images/telegramLogo.svg';
 import { Box } from '@mui/material';
 
-const topBarData =[
+const topBarData = [
     {
-        image:telegramLogo,
-        name:'Linebet_Partners_bot'
+        image: TelegramLogo,
+        name: 'Linebet_Partners_bot',
     },
     {
-        image:linkedin,
-        name:'Linebet'
+        image: LinkedIn,
+        name: 'Linebet',
     },
     {
-        image:mail,
-        name:'b2b@linebet.com'
-    },{
-        image:androidImage,
-        name:'Mobile_app_for_Android'
+        image: Mail,
+        name: 'b2b@linebet.com',
     },
-  
-]
+    {
+        image: AndroidImage,
+        name: 'Mobile_app_for_Android',
+    },
+];
 
 const TopLinkBar = () => {
     return (
-        <Box sx={{display:'flex',justifyContent:'space-around'}} >
-            {
-            topBarData.map((bar)=>
-            <Box display='flex' sx={{color:'#62ca84'}}>
-             <Box component={'img'} sx={{width:'100%',backgroundColor:'#62ca84',}} src={bar.image}></Box>
-             <Box>{bar.name} </Box>
-            </Box>)
-            }
+        <Box sx={{ display: 'flex', justifyContent: 'space-evenly' }}>
+            {topBarData.map((bar, index) => (
+                <Box key={index} display="flex" justifyContent='center'sx={{ color: '#62ca84' }}>                   
+                    <Box sx={{ width: '24px', height: '24px', svg: { fill: '#62ca84' }, mr: 1 }}>
+                        <bar.image />
+                    </Box>                    
+                    <Box fontSize='small'>{bar.name}</Box>
+                </Box>
+            ))}
         </Box>
     );
 };
